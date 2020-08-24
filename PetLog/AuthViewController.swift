a391c95fa9bd951ed56863e127bcd1d6dd603233
noelia <nzarzoso@minsait.com>
2020-08-24 16:19:49 +0200

    firebase 1

--- PetLog/AuthViewController.swift
+++ PetLog/AuthViewController.swift
@@ -8,14 +8,53 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class AuthViewController: UIViewController {
-
+    @IBOutlet weak var passText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var signupButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Autenticación"
     }
 
-
+    
+    @IBAction func signupButtonAction(_ sender: Any) {
+        if let email = emailText.text,let password = passText.text{
+            Auth.auth().createUser(withEmail: email, password: password){
+                (result,error)in
+                if let result = result, error == nil{
+                    
+                    self.navigationController?.pushViewController(HomeViewController(email: result.user.email!,provider: .basic), animated: true)
+                }else{
+                    let alert = UIAlertController(title: "Error" , message: "No se ha podido registrar el usuario" , preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    self.present(alert,animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func loginButtonAction(_ sender: Any) {
+      
+       if let email = emailText.text,let password = passText.text{
+           Auth.auth().signIn(withEmail: email, password: password){
+               (result,error)in
+               if let result = result, error == nil{
+                   
+                   self.navigationController?.pushViewController(HomeViewController(email: result.user.email!,provider: .basic), animated: true)
+               }else{
+                   let alert = UIAlertController(title: "Error" , message: "Ese usuario no está registrado" , preferredStyle: .alert)
+                   alert.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                   self.present(alert,animated: true, completion: nil)
+               }
+           }
+       }
+    }
 }
 
--- PetLog/HomeViewController.swift
+++ PetLog/HomeViewController.swift
@@ -7,23 +7,40 @@
 //
 
 import UIKit
+enum ProviderType: String{
+    case basic
+}
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var closeSesionButton: UIButton!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var providerLabel: UILabel!
+    private let email: String
+    private let provider: ProviderType
+    init(email: String, provider:ProviderType){
+        self.email = email
+        self.provider = provider
+        super.init(nibName:nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("Implementa el código de inicialización")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "Inicio"
     }
     
-
+    @IBAction func closeSesionAction(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
     */
 
